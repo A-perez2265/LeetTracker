b69b3cb86e89f1bbf3ca494a8ddd52248dbe5fb1
A-perez2265 <perez.augustine19@gmail.com>
2025-08-27 22:47:06 -0500

    File updated at 8/27/2025, 10:47:08 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,10 +1,10 @@
-# Last updated: 8/27/2025, 10:46:21 PM
+# Last updated: 8/27/2025, 10:47:08 PM
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newString = ""
-        for c in s:
-            if c.isalnum():
-                newString += c.lower()
-        return newString == newString[::-1]
+        new = ""
+        for i in s:
+            if i.isalnum():
+               new = new + i.lower()   
 
-        
+        return True if new == new[::-1] else False
+     
