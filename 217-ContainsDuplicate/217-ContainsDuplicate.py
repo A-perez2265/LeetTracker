614ecc96c6747d608191915bf2676eec34c89678
A-perez2265 <perez.augustine19@gmail.com>
2025-08-29 02:16:33 -0500

    File updated at 8/29/2025, 2:16:34 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,14 +1,4 @@
-# Last updated: 8/29/2025, 2:10:42 AM
+# Last updated: 8/29/2025, 2:16:34 AM
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        map = {}
-
-        for i, n in enumerate(nums):
-            if n in map:
-                return True
-            else:
-                map[n]=i
-        return False
-        
-
-        
+        return len(nums) != len(set(nums))
