6bf9719365b70dda0ec4275004bbf403ae9dbedf
A-perez2265 <perez.augustine19@gmail.com>
2025-11-06 14:23:29 -0600

    File updated at 11/6/2025, 2:23:29 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,14 +1,13 @@
-# Last updated: 9/5/2025, 2:46:10 AM
+# Last updated: 11/6/2025, 2:23:29 PM
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
-        stack = []
 
         for i, n in enumerate(nums):
             diff = target - n 
             if diff in map:
-                return map[diff],i
+                return [map[diff],i]
             else:
-                map[n] = i
+                map[n]=i
             
         
