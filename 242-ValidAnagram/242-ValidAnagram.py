e2dd5c66a971d3587291c560948b85bb724c97c1
A-perez2265 <perez.augustine19@gmail.com>
2025-11-06 18:30:03 -0600

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,19 @@
+# Last updated: 11/6/2025, 6:30:03 PM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS , countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+
+        return True
+            
+
+
