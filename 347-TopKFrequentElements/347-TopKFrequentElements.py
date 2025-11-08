dd071a604d342d1865700f9bece719b0508d3f3b
A-perez2265 <perez.augustine19@gmail.com>
2025-11-08 16:50:52 -0600

    File updated at 11/8/2025, 4:50:52 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,22 +1,24 @@
-# Last updated: 11/7/2025, 4:19:40 PM
+# Last updated: 11/8/2025, 4:50:52 PM
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-
-        for s in nums:
-            if s in freq:
-                freq[s] += 1
-            else:
-                freq[s] = 1
-        all_items =list(freq.items())
-
-        sorted_list = sorted(all_items , key=lambda item: item[1], reverse=True)
-
-        k_top_items = sorted_list[:k]
-
+        if not nums:
+            return []
+        
+        counts = Counter(nums)
+    
+        min_heap = []
+    
+        for num, freq in counts.items():
+            heapq.heappush(min_heap, (freq, num))
+        
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
         result = []
-
-        for item in k_top_items:
-            result.append(item[0])
+        for item_tuple in min_heap:
+            freq = item_tuple[0]  
+            num = item_tuple[1]  
+            result.append(num)   
+    
         return result
         
